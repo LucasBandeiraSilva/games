57963d08c149abe09448a09ce1006abd14f232ce
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-01-27 21:15:39 -0300

    Project Created

---
+++ src/test/java/com/lucas/bandeira/games/games/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.bandeira.games.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
